91effc221d72cf8c0cb83e7c6e03a9b979374851
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-01-15 21:54:44 +0500

    Lecture 1
    Intro to python.

---
+++ pythonintro.py
@@ -0,0 +1,16 @@
+"Hi"
+"jahanzeb"
+print("Jahanzeb")
+print(1+1)
+print(1-1)
+print ("==============")
+a = 6
+b = 22
+print(b/a)
+a = "Jahanzeb"
+b = "Naeem"
+print(a + " " + b)
+print("doesn't")
+print("py" "thon")
+square = [1, 2, 3, 5, 7]
+print(square[4])
