a4539e38802286c011734a4556e799ab270b3f3d
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-01-23 17:02:18 +0500

    Lecture 3
    Python loops, conditionals and range

---
+++ pythonintroconti2.py
@@ -0,0 +1,17 @@
+cars = ['honda', 'toyota', 'MG', 'Merc', 'Kia']
+for car in cars:
+    if car == 'toyota' or car == 'MG':
+        print(car + ' is available')
+    else:
+        print(car + ' is not available')
+for i in range(2):
+    print(i)
+print(list(range(5, 11)))
+a = ['Mary', 'had', 'a', 'little', 'lamb']
+for i in range(len(a)):
+    print(i)
+print(sum(range(4,9)))
+def febi(n):
+    for i in range(n):
+        print(i)
+febi(7)
