40f7c62fed842554a56fb3ba96f4961891049fef
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-03-13 17:30:55 +0500

    Lecture 11
    Models and faker library

--- first_project/first_project/settings.py
+++ first_project/first_project/settings.py
@@ -15,8 +15,8 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-# print(os.path.join(BASE_DIR,'templates'))
-# TEMPLATE_DIR = os.path.join(BASE_DIR,"templates")
+# print(os.path.join(BASE_DIR, "templates"))
+# TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 STATIC_DIR = os.path.join(BASE_DIR, "static")
 
 
@@ -24,7 +24,7 @@ STATIC_DIR = os.path.join(BASE_DIR, "static")
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fh=zx=@%@w_yjm!@ta@9$82+r%j#8%b1p^%*16uzs1mvcfj!n*'
+SECRET_KEY = 'django-insecure-jeg5q!9sp8e0%2eibqd$x%)hw69o!-tv4m-xipluavx(+_9gc_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'first_app',
+    'first_app'
 ]
 
 MIDDLEWARE = [
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'first_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates',],
+        'DIRS': ["templates",],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
